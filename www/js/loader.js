a50cc2ddfce15cfd866673088b25550918614b9f
Victor Recines <victor@Apple-5.local>
2017-01-03 00:53:56 -0500

    XD

--- www/js/loader.js
+++ www/js/loader.js
@@ -1,29 +1,12 @@
 document.addEventListener("deviceready", function() {
-  var pTags = document.getElementsByClassName("demo");
-  for (var i = 0; i < pTags.length; i++) {
-    executeCode(pTags[i]);
-  }
+  ready();
 }, false);
 
-function executeCode(pTag) {
-  var code = pTag.getElementsByTagName("pre")[0];
-  //console.log(code.innerText);
-  eval(code.innerText);
-}
 
-function showVirtualDialog(parentDiv, message) {
-  var virtualDialog = document.createElement("div");
-  virtualDialog.className = "virtualDialog";
-  var text = document.createElement("div");
-  text.innerText = message;
-  text.style.width="200px";
-  text.style.height="50px";
-  virtualDialog.appendChild(text);
-  virtualDialog.addEventListener("click", function() {
-    parentDiv.removeChild(virtualDialog);
-    virtualDialog.removeEventListener("click", arguments.callee);
-    virtualDialog = null;
+function ready(){
+  var div = document.getElementById("map_canvas3");
+  var map = plugin.google.maps.Map.getMap(div);
+  map.one(plugin.google.maps.event.MAP_READY, function() {
+    console.log("--> map_canvas3 : ready.");
   });
-  parentDiv.appendChild(virtualDialog);
-  return virtualDialog;
 }

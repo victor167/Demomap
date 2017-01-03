3edb441ad23411a1c730c1569b2e9bd6df0e8c7a
Victor Recines <victor@Apple-5.local>
2017-01-03 01:04:45 -0500

    XD

--- www/js/loader.js
+++ www/js/loader.js
@@ -2,11 +2,23 @@ document.addEventListener("deviceready", function() {
   ready();
 }, false);
 
-
+var map;
+var div;
 function ready(){
-  var div = document.getElementById("map_canvas3");
-  var map = plugin.google.maps.Map.getMap(div);
+  div = document.getElementById("map_canvas3");
+  map = plugin.google.maps.Map.getMap(div);
   map.one(plugin.google.maps.event.MAP_READY, function() {
-    console.log("--> map_canvas3 : ready.");
+    map_ready();
   });
+}
+
+function map_ready(){
+  map.moveCamera({
+      target: {lat: -12.0601566, lng: -77.1552091},
+      zoom: 17,
+      tilt: 60,
+      bearing: 140
+    }, function() {
+      //alert("Camera target has been changed");
+    });
 }
